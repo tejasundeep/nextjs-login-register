2c0d571c683d40eea6138a1448269c1bf72daac1
tejasundeep <tejasundeep@cryptoforce.in>
2023-06-23 11:22:52 +0530

    updated authentication

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import dbConnect from "@/database";
 
 export default NextAuth({
